6e8793a766e18c58a247f0dfbaba51ddaea47d60
krishnaprakash3 <prakashkrishna822@gmail.com>
2026-09-19 21:53:54 +0530

    Add BusDemo class for bus reservation system

---
+++ Main.java
@@ -0,0 +1,31 @@
+package busResv;
+import java.util.*;
+public class BusDemo {
+	public static void main(String[] args) {
+		ArrayList<Bus> buses=new ArrayList<Bus>();
+		ArrayList<Booking> bookings=new ArrayList<Booking>();
+		buses.add(new Bus(1,true,2));
+		buses.add(new Bus(2,true,57));
+		buses.add(new Bus(3,true,42));
+		
+		Scanner sc=new Scanner(System.in);
+		for(Bus b:buses) {
+			b.displayBusInfo();
+		}
+		int userOpt=1;
+		while(userOpt==1) {
+		System.out.println("Enter 1 to Book and 2 to Exit");
+		userOpt=sc.nextInt(); 
+		if(userOpt==1) {
+			Booking booking=new Booking();
+			if(booking.isAvailable(bookings,buses)) {
+				bookings.add(booking);
+				System.out.println("Your booking is confirmed");
+			}
+			else {
+				System.out.println("Sorry, Bus is full. Try another bus");
+			}
+		}
+	}
+	}
+}
